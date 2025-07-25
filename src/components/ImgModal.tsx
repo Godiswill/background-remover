6dddd70ced3781ba37cf4bd9d45fe0ca81920ff2
godis <isunrj@icloud.com>
2025-07-25 16:00:46 +0800

    fix: preview img

--- src/components/ImgModal.tsx
+++ src/components/ImgModal.tsx
@@ -11,8 +11,12 @@ export default function ({
   return isVisible
     ? createPortal(
         <div className="fixed inset-0 bg-black/90 flex items-center justify-center z-9999">
-          <div className="relative max-w-7xl max-h-full p-12">
-            <img src={url} alt="Preview | BgGone" className="w-full h-auto" />
+          <div className="relative flex items-center max-w-7xl h-1/2 md:h-2/3 lg:h-9/12 p-12 overflow-hidden">
+            <img
+              src={url}
+              alt="Preview | BgGone"
+              className="max-h-full max-w-full object-contain"
+            />
             <button
               className="absolute top-2 right-2 w-10 h-10 bg-white/30 text-white rounded-full hover:bg-white/40 transition-all"
               onClick={onClose}
