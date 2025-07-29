dc78758467e115737946d0c46424e5bdfa191e0b
godis <isunrj@icloud.com>
2025-07-30 01:14:22 +0800

    fix: onnxruntime-web@1.8.0 requestAdapterInfo is not function

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,50 @@ import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
+function requestAdapterInfoDev() {
+  return {
+    name: 'replace-requestAdapterInfoDev',
+    enforce: 'pre',
+    apply: 'serve',
+    transform(code, id) {
+      // if (/await\s+(\w+)\.requestAdapterInfo\(\)/.test(code)) {
+      if (/@imgly_background-removal\.js/.test(id)) {
+        console.log(id);
+        const replaced = code.replace(
+          /await\s+(\w+)\.requestAdapterInfo\(\)/g,
+          `$1.info`
+        );
+        return {
+          code: replaced,
+          map: null,
+        };
+      }
+    },
+  };
+}
+
+function requestAdapterInfoBuild() {
+  return {
+    name: 'replace-requestAdapterInfoBuild',
+    enforce: 'pre',
+    apply: 'build',
+    transform(code, id) {
+      if (/ort\.webgpu\.min\.js$/.test(id)) {
+        console.log(id);
+        const replaced = code.replace(
+          /await\s+(\w+)\.requestAdapterInfo\(\)/g,
+          // `$1.requestAdapterInfo?await $1.requestAdapterInfo():$1.info||{vendor:'unknown',architecture:'unknown',description:'shim'}`
+          `$1.info`
+        );
+        return {
+          code: replaced,
+          map: null,
+        };
+      }
+    },
+  };
+}
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bgg.one',
@@ -18,7 +62,11 @@ export default defineConfig({
   integrations: [react(), sitemap()],
 
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      requestAdapterInfoDev(),
+      requestAdapterInfoBuild(),
+    ],
     server: {
       headers: {
         'Cross-Origin-Opener-Policy': 'same-origin',
